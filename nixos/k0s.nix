f0a64b3333b556d7d1f3f63bfc0867c538d1c670
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 18:58:48 +0100

    refactor: Use mkEnableOption to generate the enable option

--- nixos/k0s.nix
+++ nixos/k0s.nix
@@ -7,13 +7,7 @@ let cfg = config.services.k0s;
 in {
 
   options.services.k0s = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Enable the k0s Kubernetes distribution.
-      '';
-    };
+    enable = mkEnableOption (lib.mdDoc "Enable the k0s Kubernetes distribution.");
 
     role = mkOption {
       type = types.enum [ "controller" "controller+worker" "worker" "single"];
