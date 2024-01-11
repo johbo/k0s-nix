87c66463e87f65fd4c0b66bc3f84000a5934b253
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 16:09:26 +0100

    feat: Add the Nix flake template

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "k0s - The Zero Friction Kubernetes for NixOS";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
