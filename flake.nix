4d1e88f45ab0795f2e9439ca12bb1f34f7b2c119
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 18:40:51 +0100

    feat: Add overlay

--- flake.nix
+++ flake.nix
@@ -21,6 +21,12 @@
           }
         );
 
+    overlays.default = final: prev: rec {
+      inherit (prev.callPackage ./k0s/default.nix {})
+        k0s_1_28;
+      k0s = k0s_1_28;
+    };
+
     nixosConfigurations = {
       test = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
