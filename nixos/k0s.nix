fd91680bad2caad12d57779a400a1357b82bd714
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 20:10:48 +0100

    feat: Add service unit configuration

--- nixos/k0s.nix
+++ nixos/k0s.nix
@@ -22,8 +22,40 @@ in {
   };
 
 
-  config = mkIf cfg.enable {
-
-  };
+  config =
+    let
+      subcommand = if (cfg.role == "worker") then "worker" else "controller";
+      unitName = "k0s" + subcommand;
+    in
+    {
+      systemd.services.${unitName} = {
+        description = "k0s - Zero Friction Kubernetes";
+        documentation = [ "https://docs.k0sproject.io" ];
+        path = with pkgs; [
+          kmod
+          util-linux
+          mount
+        ];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        wantedBy = [ "multi-user.target" ];
+        startLimitIntervalSec = 5;
+        startLimitBurst = 10;
+        serviceConfig = {
+          RestartSec = 120;
+          Delegate = "yes";
+          KillMode = "process";
+          LimitCORE = "infinity";
+          TasksMax = "infinity";
+          TimeoutStartSec = 0;
+          LimitNOFILE = 999999;
+          Restart = "always";
+          ExecStart = "${cfg.package}/bin/k0s ${subcommand} --config=/etc/k0s/k0s.yaml --data-dir=/var/lib/k0s"
+            + optionalString (cfg.role == "single") " --single"
+            + optionalString (cfg.role == "controller+worker") " --enable-worker"
+            + optionalString (cfg.role == "worker") " --token-file=/etc/k0s/k0stoken";
+        };
+      };
+    };
 
 }
