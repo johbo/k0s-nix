ba6f1b9043bff9740795da1a8bbbff78735a6743
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 18:05:40 +0100

    feat: Add option "role" to the nixos module

--- nixos/k0s.nix
+++ nixos/k0s.nix
@@ -14,6 +14,15 @@ in {
         Enable the k0s Kubernetes distribution.
       '';
     };
+
+    role = mkOption {
+      type = types.enum [ "controller" "controller+worker" "worker" "single"];
+      default = "single";
+      description = ''
+        The role of the node.
+      '';
+    };
+
   };
 
 
