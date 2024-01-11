3706c1215d39458811126092db428f574fbddf91
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 16:29:15 +0100

    feat: Pin nixpkgs to nixos-23.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "k0s - The Zero Friction Kubernetes for NixOS";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
   outputs = { self, nixpkgs }: {
 
     nixosModules = {
