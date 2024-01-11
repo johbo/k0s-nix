c88a8d5ab06e59f8fe2a00a18be78d98bbd0eebe
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 16:30:06 +0100

    feat: Define a test NixOS configuration

--- flake.nix
+++ flake.nix
@@ -5,6 +5,21 @@
 
   outputs = { self, nixpkgs }: {
 
+    nixosConfigurations = {
+      test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./k0s-module.nix
+          ({pkgs, ... }: {
+            boot.isContainer = true;
+
+            services.k0s.enable = true;
+          })
+        ];
+      };
+    };
+
     nixosModules = {
       k0s = import ./k0s-module.nix;
     };
