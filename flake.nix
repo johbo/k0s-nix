373f3fa7c98c5df5ef61f4cd6a32d2d646530b5b
Johannes Bornhold <johannes@bornhold.name>
2024-01-11 23:38:08 +0100

    refactor: Move import of packages into a function

--- flake.nix
+++ flake.nix
@@ -3,60 +3,62 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-  outputs = { self, nixpkgs }: rec {
+  outputs = { self, nixpkgs }:
+    let
 
-    packages =
-      let
-        lib = nixpkgs.lib;
-        allSystems = [ "armv7l-linux" "aarch64-linux" "x86_64-linux" ];
-        forAllSystems = lib.genAttrs allSystems;
-      in
-        forAllSystems (system:
-          let
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          in rec {
-            inherit (pkgs.callPackage ./k0s/default.nix {})
-              k0s_1_28;
-            k0s = k0s_1_28;
-          }
-        );
-
-    overlays.default = final: prev: rec {
-      inherit (prev.callPackage ./k0s/default.nix {})
-        k0s_1_28;
-      k0s = k0s_1_28;
-    };
-
-    nixosConfigurations = {
-      test = nixpkgs.lib.nixosSystem {
-        modules = [
-          ({
-            nixpkgs.system = "x86_64-linux";
-            nixpkgs.pkgs = import nixpkgs {
-              system = "x86_64-linux";
-              overlays = [
-                overlays.default
-              ];
-            };
-          })
-          ./nixos/k0s.nix
-          ({pkgs, ... }: {
-            boot.isContainer = true;
-
-            services.k0s = {
-              enable = true;
-              apiAddress = "192.0.2.1";
-              apiSans = [
-                "192.0.2.1"
-                "192.0.2.2"
-              ];
-            };
-          })
-        ];
+      genPackages = pkgs: rec {
+        inherit (pkgs.callPackage ./k0s/default.nix {})
+          k0s_1_28;
+        k0s = k0s_1_28;
       };
-    };
 
-    nixosModules.default = import ./nixos/k0s.nix;
+    in
+    rec {
+      packages =
+        let
+          lib = nixpkgs.lib;
+          allSystems = [ "armv7l-linux" "aarch64-linux" "x86_64-linux" ];
+          forAllSystems = lib.genAttrs allSystems;
+        in
+          forAllSystems (system:
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+            in genPackages pkgs
+          );
 
+      overlays.default = final: prev: genPackages prev;
+
+      nixosConfigurations = {
+        test = nixpkgs.lib.nixosSystem {
+          modules = [
+            ({
+              nixpkgs.system = "x86_64-linux";
+              nixpkgs.pkgs = import nixpkgs {
+                system = "x86_64-linux";
+                overlays = [
+                  overlays.default
+                ];
+              };
+            })
+            ./nixos/k0s.nix
+            ({pkgs, ... }: {
+              boot.isContainer = true;
+
+              services.k0s = {
+                enable = true;
+                role = "worker";
+
+                apiAddress = "192.0.2.1";
+                apiSans = [
+                  "192.0.2.1"
+                  "192.0.2.2"
+                ];
+              };
+            })
+          ];
+        };
+      };
+
+      nixosModules.default = import ./nixos/k0s.nix;
   };
 }
