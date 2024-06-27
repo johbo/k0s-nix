17ad912428f322798e4f9aac7cf97487a6a6d785
Jeremias Weber <jeremias.weber@jelemux.dev>
2024-06-27 17:49:22 +0200

    Extract kube-proxy into submodule

---
+++ nixos/kubeproxy.nix
@@ -0,0 +1,83 @@
+{ lib, ... }@args: let
+  inherit (lib) mkEnableOption mkOption;
+  inherit (lib.types) str enum bool nullOr attrsOf listOf attrTag int;
+in {
+  options = {
+    disabled = mkEnableOption ''
+      Disable kube-proxy altogether (default: `false`).
+    '';
+
+    mode = mkOption {
+      type = enum [ "iptables" "ipvs" "userspace" ];
+      default = "iptables";
+      description = ''
+        Kube proxy operating mode, supported modes iptables, ipvs, userspace (default: iptables).
+      '';
+    };
+
+    iptables = mkOption {
+      type = nullOr (attrsOf (attrTag {
+        masqueradeBit = mkOption {
+          type = nullOr int;
+        };
+        masqueradeAll = mkOption {
+          type = bool;
+        };
+        localhostNodePorts = mkOption {
+          type = nullOr bool;
+        };
+        syncPeriod = mkOption {
+          type = str;
+        };
+        minSyncPeriod = mkOption {
+          type = str;
+        };
+      }));
+      default = null;
+      description = ''
+        Kube proxy iptables settings.
+      '';
+    };
+
+    ipvs = mkOption {
+      type = nullOr (attrsOf (attrTag {
+        syncPeriod = mkOption {
+          type = str;
+        };
+        minSyncPeriod = mkOption {
+          type = str;
+        };
+        scheduler = mkOption {
+          type = str;
+        };
+        excludedCIDRs = mkOption {
+          type = nullOr (listOf str);
+        };
+        strictARP = mkOption {
+          type = bool;
+        };
+        tcpTimeout = mkOption {
+          type = str;
+        };
+        tcpFinTimeout = mkOption {
+          type = str;
+        };
+        udpTimeout = mkOption {
+          type = str;
+        };
+      }));
+      default = null;
+      description = ''
+        Kube proxy ipvs settings.
+      '';
+    };
+
+    nodePortAddresses = mkOption {
+      type = nullOr (listOf str);
+      default = null;
+      description = ''
+        Kube proxy [nodePortAddresses](https://kubernetes.io/docs/reference/command-line-tools-reference/kube-proxy/).
+      '';
+    };
+  };
+}
--- nixos/network.nix
+++ nixos/network.nix
@@ -1,7 +1,6 @@
 { lib, config, ... }@args: let
   inherit (lib) mkEnableOption mkOption optionalAttrs;
-  inherit (lib.types) str enum bool nullOr attrsOf listOf port attrTag ints int submodule addCheck;
-  util = import ./util.nix args;
+  inherit (lib.types) str enum nullOr attrsOf listOf port attrTag ints submodule addCheck;
 in {
   options = {
 
@@ -76,87 +75,10 @@ in {
       default = {};
     });
 
-    kubeProxy = {
-      disabled = mkOption {
-        type = bool;
-        default = false;
-        description = ''
-          Disable kube-proxy altogether (default: `false`).
-        '';
-      };
-
-      mode = mkOption {
-        type = enum [ "iptables" "ipvs" "userspace" ];
-        default = "iptables";
-        description = ''
-          Kube proxy operating mode, supported modes iptables, ipvs, userspace (default: iptables).
-        '';
-      };
-
-      iptables = mkOption {
-        type = nullOr (attrsOf (attrTag {
-          masqueradeBit = mkOption {
-            type = nullOr int;
-          };
-          masqueradeAll = mkOption {
-            type = bool;
-          };
-          localhostNodePorts = mkOption {
-            type = nullOr bool;
-          };
-          syncPeriod = mkOption {
-            type = str;
-          };
-          minSyncPeriod = mkOption {
-            type = str;
-          };
-        }));
-        default = null;
-        description = ''
-          Kube proxy iptables settings.
-        '';
-      };
-
-      ipvs = mkOption {
-        type = nullOr (attrsOf (attrTag {
-          syncPeriod = mkOption {
-            type = str;
-          };
-          minSyncPeriod = mkOption {
-            type = str;
-          };
-          scheduler = mkOption {
-            type = str;
-          };
-          excludedCIDRs = mkOption {
-            type = nullOr (listOf str);
-          };
-          strictARP = mkOption {
-            type = bool;
-          };
-          tcpTimeout = mkOption {
-            type = str;
-          };
-          tcpFinTimeout = mkOption {
-            type = str;
-          };
-          udpTimeout = mkOption {
-            type = str;
-          };
-        }));
-        default = null;
-        description = ''
-          Kube proxy ipvs settings.
-        '';
-      };
-
-      nodePortAddresses = mkOption {
-        type = nullOr (listOf str);
-        default = null;
-        description = ''
-          Kube proxy [nodePortAddresses](https://kubernetes.io/docs/reference/command-line-tools-reference/kube-proxy/).
-        '';
-      };
+    kubeProxy = mkOption {
+      description = "Defines the configuration for kube-proxy.";
+      type = submodule (import ./kubeproxy.nix);
+      default = {};
     };
 
     nodeLocalLoadBalancing = {
