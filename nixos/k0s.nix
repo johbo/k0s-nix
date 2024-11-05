15bf5bf617262006c630b7989eb5669a4de99c93
Brian Logan <mail@brianjlogan.com>
2024-11-04 21:43:57 -0500

    More experimentation

--- nixos/k0s.nix
+++ nixos/k0s.nix
@@ -93,7 +93,13 @@ in {
       environment.etc = {
         "k0s/k0s.yaml".source = configFile;
       } // 
-      builtins.listToAttrs cfg.spec.extensions.manifests;
+      builtins.listToAttrs (map (manifest: {
+        name = manifest.name;
+        value = {
+          text = manifest.text;
+          destination = "/var/lib/k0s/manifests/${manifest.name}";
+        };
+      })) cfg.spec.extensions.manifests;
       
       systemd.tmpfiles.rules = [
         "d /var/lib/k0s/manifests 0755 k0s k0s -"
