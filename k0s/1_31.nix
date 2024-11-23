545b03a237468fef33b02249259a67c2b2b540e2
Philippe Laflamme <philippe.laflamme@gmail.com>
2024-11-22 23:54:11 -0500

    feat(update): k0s -> 1.31.2+k0s.0

--- k0s/1_31.nix
+++ k0s/1_31.nix
@@ -1,17 +1,17 @@
-rec {
-  version = "v1.31.1+k0s.1";
+{
+  version = "1.31.2+k0s.0";
   srcs = {
     armv7l-linux = {
-      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.1+k0s.1/k0s-v1.31.1+k0s.1-arm";
-      hash = "sha256-6sBRVu/2j7DX6TShHxNXc3hRlNpGQdcv0T8ThAZa8zM=";
+      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.2+k0s.0/k0s-v1.31.2+k0s.0-arm";
+      hash = "sha256-T2W95NwTxHlUrWffEjXRsEw+nGByYdJsYJguC+J1yIY=";
     };
     aarch64-linux = {
-      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.1+k0s.1/k0s-v1.31.1+k0s.1-arm64";
-      hash = "sha256-ua1fx5o13ei8eujDuDUuRTfrNvqwjlRM3ODviABLp4U=";
+      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.2+k0s.0/k0s-v1.31.2+k0s.0-arm64";
+      hash = "sha256-DR1Qm0NFXwyKJTImh+Csj0tgk4M76RycHTMI/t7sbOs=";
     };
     x86_64-linux = {
-      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.1+k0s.1/k0s-v1.31.1+k0s.1-amd64";
-      hash = "sha256-mhL8Qg0Jy3dkJ7TIA78rBEWzTQUXsFRzJKnJprNRjGg=";
+      url = "https://github.com/k0sproject/k0s/releases/download/v1.31.2+k0s.0/k0s-v1.31.2+k0s.0-amd64";
+      hash = "sha256-wY3VpSLpj6lDx18AnQ1jTtCIuydgaGX7R5qUtkFRIIU=";
     };
   };
 }
