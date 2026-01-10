8c05408286609bca1e1a66532c9a26fe101d1479
Omar Mohamed Khallaf <mohamed.omar67492@gmail.com>
2026-01-10 13:59:39 +0000

    fix: no conditions are actually needed
    Signed-off-by: Omar Mohamed Khallaf <mohamed.omar67492@gmail.com>

--- nixos/k0s.nix
+++ nixos/k0s.nix
@@ -90,7 +90,7 @@ in
       type = str;
     };
 
-    extraServiceArgs = mkOption {
+    extraArgs = mkOption {
       description = ''
         Extra arguments to pass to systemd ExecStart
       '';
@@ -148,7 +148,7 @@ in
             + optionalString (cfg.role == "single") " --single"
             + optionalString (cfg.role == "controller+worker") " --enable-worker --no-taints"
             + optionalString requireJoinToken " --token-file=${cfg.tokenFile}"
-            + optionalString (cfg.extraServiceArgs != "") cfg.extraServiceArgs;
+            + " ${cfg.extraServiceArgs}";
         };
         unitConfig = mkIf requireJoinToken { ConditionPathExists = cfg.tokenFile; };
       };
