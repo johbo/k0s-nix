54af3a66fb13bcf56ff6982e83b8743a92552169
Johannes Bornhold <johannes@bornhold.name>
2026-08-24 18:09:37 +0200

    feat: Link option declarations to the repository

--- checks/docs.nix
+++ checks/docs.nix
@@ -4,6 +4,27 @@
   module,
 }:
 let
+  inherit (pkgs) lib;
+
+  sourceRoot = "${toString ../.}/";
+  repository = "https://github.com/nix-community/k0s-nix/blob/main";
+
+  linkToRepository =
+    declaration:
+    let
+      path = toString declaration;
+    in
+    if lib.hasPrefix sourceRoot path then
+      let
+        relative = lib.removePrefix sourceRoot path;
+      in
+      {
+        name = relative;
+        url = "${repository}/${relative}";
+      }
+    else
+      declaration;
+
   eval = import (nixpkgs + "/nixos/lib/eval-config.nix") {
     inherit pkgs;
     # Dropping this falls back to builtins.currentSystem, which is
@@ -14,6 +35,12 @@ let
 
   doc = pkgs.nixosOptionsDoc {
     options = eval.options.services.k0s;
+    transformOptions =
+      option:
+      option
+      // {
+        declarations = map linkToRepository option.declarations;
+      };
     # TODO: Drop this once every option carries a description
     warningsAreErrors = false;
   };
