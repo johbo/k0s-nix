163175d912ae211f637b6c1ad296e03b2de0fe63
Johannes Bornhold <johannes@bornhold.name>
2026-08-30 18:04:41 +0200

    docs: Say why the pytest cache is turned off

--- checks/embedded-bins-update.nix
+++ checks/embedded-bins-update.nix
@@ -6,6 +6,8 @@ pkgs.runCommand "k0s-embedded-bins-update-tests"
   }
   ''
     cd ${../k0s/embedded-bins}
+    # Without this pytest warns on every run: the cache would land beside the
+    # tests, in a store path.
     pytest --verbose -p no:cacheprovider
     touch $out
   ''
